a58976cc82fd387e1f7c01cab6d6e28b0a41a502
Matthew <matthew6086888@hotmail.com>
2021-06-02 17:09:00 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let activity, snacks, day;
+const activityList = ['go on a picnic', 'watch a movie', 'play at the playground', 'go to a restaurant', 'bake brownies', 'bake bannana bread', 'play in the bathtub', 'play Minecraft', 'play Roblox', 'make bubble tea'];
+const snacksList = ['chips', 'gummies', 'sushi', 'munchies', 'spring rolls', 'chicken karaage', 'katsudon'];
+const dayList = ['Friday', 'Saturday', 'Sunday'];
+
+const generateMessage = () => {
+    activity = activityList[Math.floor(Math.random() * activityList.length)];
+    snacks = snacksList[Math.floor(Math.random() * snacksList.length)];
+    day = dayList[Math.floor(Math.random() * dayList.length)];
+    return `This weekend we should get some ${snacks} and ${activity}! Doesn't that sound fun? Let's do it this ${day}!`;
+}
+
+console.log(generateMessage());
